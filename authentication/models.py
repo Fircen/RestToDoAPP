dfec9b01b033562bea6e16ff20f813fd7208c761
Maciej Leszczyński <maciek15557@gmail.com>
2022-10-26 16:38:59 +0200

    Project Start

---
+++ authentication/models.py
@@ -0,0 +1,46 @@
+from typing import Type
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password = None, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if email is None:
+            raise TypeError('User should have a email') 
+    
+        user= self.model(email=self.normalize_email(email),**extra_fields )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    username = None
+    email = models.EmailField(max_length = 255, unique = True)
+    is_verified = models.BooleanField(default = False)
+    is_staff = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
+    create_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    object = UserManager()
+
+    def __str__(self):
+        return self.email
